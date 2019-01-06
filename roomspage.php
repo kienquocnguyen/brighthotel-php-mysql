17c134060375f2740251b92ca2d66164b2a60cb3
DESKTOP-NU7UFKN\DELL <kienquocnguyen@gmail.com>
2019-01-06 13:09:16 +0700

    1:09PM 6/1/2019

--- roomspage.php
+++ roomspage.php
@@ -1,6 +1,24 @@
 <?php include_once("includes/header.php") ?>
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">BrightHotel</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
 
-
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+          <li class="nav-item active"><a href="roomspage.php" class="nav-link">Rooms</a></li>
+          <li class="nav-item"><a href="bookingpage.php" class="nav-link">Booking</a></li>
+          <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
+          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- END nav -->
 <div class="block-30 block-30-sm item" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
     <div class="container">
       <div class="row align-items-center">
